4b436b40960d8642433c8c2a19ac46db06bafa0d
Anish Abraham <cuxtud@gmail.com>
2025-01-28 17:38:22 +0000

    added te resouce for all ipspaces to be allocated

--- createOrg/t1_edge_gayeway.tf
+++ createOrg/t1_edge_gayeway.tf
@@ -68,6 +68,63 @@ resource "vcd_ip_space_ip_allocation" "default_ip_prefix" {
   type        = "IP_PREFIX"
   value       = "10.0.0.0/24"  
 }
+
+resource "vcd_ip_space_ip_allocation" "172_16_0_0_24" {
+  org_id      = vcd_org.my-org.id
+  ip_space_id = vcd_ip_space.space1.id
+  type        = "IP_PREFIX"
+  value       = "172.16.0.0/24"  
+}
+
+resource "vcd_ip_space_ip_allocation" "192_168_0_0_24" {
+  org_id      = vcd_org.my-org.id
+  ip_space_id = vcd_ip_space.space1.id
+  type        = "IP_PREFIX"
+  value       = "192.168.0.0/24"  
+}
+
+resource "vcd_ip_space_ip_allocation" "192_168_50_0_25" {
+  org_id      = vcd_org.my-org.id
+  ip_space_id = vcd_ip_space.space1.id
+  type        = "IP_PREFIX"
+  value       = "192.168.50.0/25"  
+}
+
+resource "vcd_ip_space_ip_allocation" "172_17_0_0_25" {
+  org_id      = vcd_org.my-org.id
+  ip_space_id = vcd_ip_space.space1.id
+  type        = "IP_PREFIX"
+  value       = "172.17.0.0/25"  
+}
+
+resource "vcd_ip_space_ip_allocation" "10_1_0_0_25" {
+  org_id      = vcd_org.my-org.id
+  ip_space_id = vcd_ip_space.space1.id
+  type        = "IP_PREFIX"
+  value       = "10.1.0.0/25"  
+}
+
+resource "vcd_ip_space_ip_allocation" "192_168_100_0_26" {
+  org_id      = vcd_org.my-org.id
+  ip_space_id = vcd_ip_space.space1.id
+  type        = "IP_PREFIX"
+  value       = "192.168.100.0/26"  
+}
+
+resource "vcd_ip_space_ip_allocation" "172_18_0_0_26" {
+  org_id      = vcd_org.my-org.id
+  ip_space_id = vcd_ip_space.space1.id
+  type        = "IP_PREFIX"
+  value       = "172.18.0.0/26"  
+}
+
+resource "vcd_ip_space_ip_allocation" "10_2_0_0_26" {
+  org_id      = vcd_org.my-org.id
+  ip_space_id = vcd_ip_space.space1.id
+  type        = "IP_PREFIX"
+  value       = "10.2.0.0/26"  
+}
+
 #Add default router network to the vcd
 resource "vcd_network_routed_v2" "default-routed-network" {
   org         = data.vcd_org.org1.name
