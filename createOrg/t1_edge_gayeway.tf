705d91a03566d84283e12e01449e879c6d6950c3
Anish Abraham <cuxtud@gmail.com>
2025-01-28 17:54:05 +0000

    updated names of the prefix resources

--- createOrg/t1_edge_gayeway.tf
+++ createOrg/t1_edge_gayeway.tf
@@ -69,56 +69,56 @@ resource "vcd_ip_space_ip_allocation" "default_ip_prefix" {
   value       = "10.0.0.0/24"  
 }
 
-resource "vcd_ip_space_ip_allocation" "172_16_0_0_24" {
+resource "vcd_ip_space_ip_allocation" "_172_16_0_0_24" {
   org_id      = vcd_org.my-org.id
   ip_space_id = vcd_ip_space.space1.id
   type        = "IP_PREFIX"
   value       = "172.16.0.0/24"  
 }
 
-resource "vcd_ip_space_ip_allocation" "192_168_0_0_24" {
+resource "vcd_ip_space_ip_allocation" "_192_168_0_0_24" {
   org_id      = vcd_org.my-org.id
   ip_space_id = vcd_ip_space.space1.id
   type        = "IP_PREFIX"
   value       = "192.168.0.0/24"  
 }
 
-resource "vcd_ip_space_ip_allocation" "192_168_50_0_25" {
+resource "vcd_ip_space_ip_allocation" "_192_168_50_0_25" {
   org_id      = vcd_org.my-org.id
   ip_space_id = vcd_ip_space.space1.id
   type        = "IP_PREFIX"
   value       = "192.168.50.0/25"  
 }
 
-resource "vcd_ip_space_ip_allocation" "172_17_0_0_25" {
+resource "vcd_ip_space_ip_allocation" "_172_17_0_0_25" {
   org_id      = vcd_org.my-org.id
   ip_space_id = vcd_ip_space.space1.id
   type        = "IP_PREFIX"
   value       = "172.17.0.0/25"  
 }
 
-resource "vcd_ip_space_ip_allocation" "10_1_0_0_25" {
+resource "vcd_ip_space_ip_allocation" "_10_1_0_0_25" {
   org_id      = vcd_org.my-org.id
   ip_space_id = vcd_ip_space.space1.id
   type        = "IP_PREFIX"
   value       = "10.1.0.0/25"  
 }
 
-resource "vcd_ip_space_ip_allocation" "192_168_100_0_26" {
+resource "vcd_ip_space_ip_allocation" "_192_168_100_0_26" {
   org_id      = vcd_org.my-org.id
   ip_space_id = vcd_ip_space.space1.id
   type        = "IP_PREFIX"
   value       = "192.168.100.0/26"  
 }
 
-resource "vcd_ip_space_ip_allocation" "172_18_0_0_26" {
+resource "vcd_ip_space_ip_allocation" "_172_18_0_0_26" {
   org_id      = vcd_org.my-org.id
   ip_space_id = vcd_ip_space.space1.id
   type        = "IP_PREFIX"
   value       = "172.18.0.0/26"  
 }
 
-resource "vcd_ip_space_ip_allocation" "10_2_0_0_26" {
+resource "vcd_ip_space_ip_allocation" "_10_2_0_0_26" {
   org_id      = vcd_org.my-org.id
   ip_space_id = vcd_ip_space.space1.id
   type        = "IP_PREFIX"
